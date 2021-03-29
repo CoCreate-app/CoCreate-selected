b8f6b22cb9e65abb56c10c23c4dfd3f843df9043
Frank <frank@cocreate.app>
2021-03-29 22:15:41 +0000

    clean tree

--- src/index.js
+++ src/index.js
@@ -1,148 +1,148 @@
 const CoCreateSelected = {
 
 
-	init: function() {
-		this.initElement(document);
-	},
+	// init: function() {
+	// 	this.initElement(document);
+	// },
 
-	initElement: function(container) {
-		let mainContainer = container || document;
+	// initElement: function(container) {
+	// 	let mainContainer = container || document;
 
-		if (!mainContainer.querySelectorAll) {
-			return;
-		}
+	// 	if (!mainContainer.querySelectorAll) {
+	// 		return;
+	// 	}
 
-		let elements = mainContainer.querySelectorAll(`[data-selected]`);
-		if (elements.length === 0 && mainContainer != document && mainContainer.hasAttribute(`[data-selected]`)) {
-			elements = [mainContainer];
-		}
-		const self = this;
-		elements.forEach((element) => self.__initElementEvent(element));
-	},
+	// 	let elements = mainContainer.querySelectorAll(`[data-selected]`);
+	// 	if (elements.length === 0 && mainContainer != document && mainContainer.hasAttribute(`[data-selected]`)) {
+	// 		elements = [mainContainer];
+	// 	}
+	// 	const self = this;
+	// 	elements.forEach((element) => self.__initElementEvent(element));
+	// },
 
-	__initElementEvent: function(element) {
-		const selectedValue = element.getAttribute('data-selected') || "";
-		let values = selectedValue.split(',');
-		if (!values || values.length === 0) {
-			return;
-		}
-		values = values.map(x => x.trim())
+	// __initElementEvent: function(element) {
+	// 	const selectedValue = element.getAttribute('data-selected') || "";
+	// 	let values = selectedValue.split(',');
+	// 	if (!values || values.length === 0) {
+	// 		return;
+	// 	}
+	// 	values = values.map(x => x.trim())
 
-		const self = this;
+	// 	const self = this;
 
-		// if (CoCreate.observer.getInitialized(element)) {
-		// 	return;
-		// }
-		// CoCreate.observer.setInitialized(element)
-		element['co_initialized_'] = true;
+	// 	// if (CoCreate.observer.getInitialized(element)) {
+	// 	// 	return;
+	// 	// }
+	// 	// CoCreate.observer.setInitialized(element)
+	// 	element['co_initialized_'] = true;
 
-		element.addEventListener('click', function() {
-			self.__changeElementStatus(element, values)
-		});
+	// 	element.addEventListener('click', function() {
+	// 		self.__changeElementStatus(element, values)
+	// 	});
 
-		document.addEventListener('click', function(event) {
-			if (!element.hasAttribute("data-selected_group") && !element.contains(event.target)) {
+	// 	document.addEventListener('click', function(event) {
+	// 		if (!element.hasAttribute("data-selected_group") && !element.contains(event.target)) {
 
-				self.__removeSelectedStatus(element, values);
-			}
-		})
-	},
+	// 			self.__removeSelectedStatus(element, values);
+	// 		}
+	// 	})
+	// },
 
-	__changeElementStatus: function(element, values) {
-		let target_attribute = element.dataset[`selected_attribute`] || 'class';
-		let elements = this.__getTargetElements(element);
-		const self = this;
+	// __changeElementStatus: function(element, values) {
+	// 	let target_attribute = element.dataset[`selected_attribute`] || 'class';
+	// 	let elements = this.__getTargetElements(element);
+	// 	const self = this;
 
-		let selectedGroup = element.dataset['selected_group'];
-		let group = selectedGroup ? `[data-selected_group="${selectedGroup}"]` : ':not([data-selected_group])';
+	// 	let selectedGroup = element.dataset['selected_group'];
+	// 	let group = selectedGroup ? `[data-selected_group="${selectedGroup}"]` : ':not([data-selected_group])';
 
-		let previouSelected = document.querySelector('[data-selected]' + group + '[selected]');
+	// 	let previouSelected = document.querySelector('[data-selected]' + group + '[selected]');
 
-		// if (previouSelected.isSameNode(element)) {
-		// 	return ;
-		// }
+	// 	// if (previouSelected.isSameNode(element)) {
+	// 	// 	return ;
+	// 	// }
 
-		if (previouSelected) {
-			let previousValues = previouSelected.dataset['selected'].split(',').map(x => x.trim());
-			this.__removeSelectedStatus(previouSelected, previousValues)
-		}
+	// 	if (previouSelected) {
+	// 		let previousValues = previouSelected.dataset['selected'].split(',').map(x => x.trim());
+	// 		this.__removeSelectedStatus(previouSelected, previousValues)
+	// 	}
 
-		values = values.map(x => x.trim());
-		elements.forEach((el) => {
-			self.setValue(el, target_attribute, values);
-		})
-	},
+	// 	values = values.map(x => x.trim());
+	// 	elements.forEach((el) => {
+	// 		self.setValue(el, target_attribute, values);
+	// 	})
+	// },
 
-	__removeSelectedStatus: function(element, values) {
-		let attrName = element.dataset[`selected_attribute`] || 'class';
+	// __removeSelectedStatus: function(element, values) {
+	// 	let attrName = element.dataset[`selected_attribute`] || 'class';
 
-		let elements = this.__getTargetElements(element);
+	// 	let elements = this.__getTargetElements(element);
 
-		elements.forEach(el => {
-			if (attrName === 'class') {
-				let attrValues = (el.getAttribute(attrName) || "").split(' ').map(x => x.trim());
-				let currentValue = values.filter(x => attrValues.includes(x))[0] || '';
-				if (currentValue) {
-					el.classList.remove(currentValue);
-				}
-			}
-			else {
-				el.setAttribute(attrName, "");
-			}
-			el.removeAttribute('selected');
-		})
+	// 	elements.forEach(el => {
+	// 		if (attrName === 'class') {
+	// 			let attrValues = (el.getAttribute(attrName) || "").split(' ').map(x => x.trim());
+	// 			let currentValue = values.filter(x => attrValues.includes(x))[0] || '';
+	// 			if (currentValue) {
+	// 				el.classList.remove(currentValue);
+	// 			}
+	// 		}
+	// 		else {
+	// 			el.setAttribute(attrName, "");
+	// 		}
+	// 		el.removeAttribute('selected');
+	// 	})
 
-	},
+	// },
 
-	setValue: function(element, attrName, values) {
-		let currentAttrValue = element.getAttribute(attrName) || "";
-		let attrValues = currentAttrValue;
-		if (attrName === 'class') {
-			attrValues = currentAttrValue.split(' ').map(x => x.trim());
-		}
+	// setValue: function(element, attrName, values) {
+	// 	let currentAttrValue = element.getAttribute(attrName) || "";
+	// 	let attrValues = currentAttrValue;
+	// 	if (attrName === 'class') {
+	// 		attrValues = currentAttrValue.split(' ').map(x => x.trim());
+	// 	}
 
-		let oldValue = values.filter(x => attrValues.includes(x))[0] || '';
-		let newValue = this.__getNextValue(values, oldValue)
+	// 	let oldValue = values.filter(x => attrValues.includes(x))[0] || '';
+	// 	let newValue = this.__getNextValue(values, oldValue)
 
-		element.setAttribute('selected', "")
+	// 	element.setAttribute('selected', "")
 
-		if (oldValue === newValue) {
-			return;
-		}
+	// 	if (oldValue === newValue) {
+	// 		return;
+	// 	}
 
-		if (attrName === 'class') {
-			if (oldValue != '') {
-				element.classList.remove(oldValue);
-			}
-			if (newValue != '') {
-				element.classList.add(newValue);
-			}
-		}
-		else {
-			element.setAttribute(attrName, newValue);
-		}
-	},
+	// 	if (attrName === 'class') {
+	// 		if (oldValue != '') {
+	// 			element.classList.remove(oldValue);
+	// 		}
+	// 		if (newValue != '') {
+	// 			element.classList.add(newValue);
+	// 		}
+	// 	}
+	// 	else {
+	// 		element.setAttribute(attrName, newValue);
+	// 	}
+	// },
 
-	__getTargetElements: function(element) {
-		let targetSelector = element.dataset[`selected_target`];
-		let elements = [element];
-		if (targetSelector) {
-			elements = Array.from(document.querySelectorAll(targetSelector));
-			elements.push(element)
-		}
-		return elements;
-	},
+	// __getTargetElements: function(element) {
+	// 	let targetSelector = element.dataset[`selected_target`];
+	// 	let elements = [element];
+	// 	if (targetSelector) {
+	// 		elements = Array.from(document.querySelectorAll(targetSelector));
+	// 		elements.push(element)
+	// 	}
+	// 	return elements;
+	// },
 
-	__getNextValue: function(values, val) {
-		let size = values.length;
-		let nn = values.indexOf(val);
-		if (nn == -1) {
-			return values[0];
-		}
-		else {
-			return values[(nn + 1) % size];
-		}
-	},
+	// __getNextValue: function(values, val) {
+	// 	let size = values.length;
+	// 	let nn = values.indexOf(val);
+	// 	if (nn == -1) {
+	// 		return values[0];
+	// 	}
+	// 	else {
+	// 		return values[(nn + 1) % size];
+	// 	}
+	// },
 
 	config: function({
 		srcDocument,
